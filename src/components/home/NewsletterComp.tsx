97f3fd633e2bb880b2ea1d4b02e60b170d61766b
iysaofficial <youngscientist.iysa@gmail.com>
2025-09-02 11:37:56 +0700

    newsletter

--- src/components/home/NewsletterComp.tsx
+++ src/components/home/NewsletterComp.tsx
@@ -1,4 +1,37 @@
+"use client";
+import React, { useEffect } from "react";
+
 const NewsletterComp = () =>{
+    useEffect(() => {
+        const scriptURL =
+        "https://script.google.com/macros/s/AKfycbwfw1dAhzyY6p6crVCCuoEXzb_E7hBOi8cYuIBu_-JZzz2a25h-tCvT6Xd3dUH9Mx2fQA/exec";
+
+        const form = document.forms.namedItem("regist-form");
+
+        if (form) {
+        const handleSubmit = async (e: Event) => {
+            e.preventDefault();
+            try {
+            await fetch(scriptURL, {
+                method: "POST",
+                body: new FormData(form),
+            });
+            alert("Data sent successfully!");
+            form.reset();
+            } catch (error) {
+            console.error("Error:", error);
+            alert("Failed to send data.");
+            }
+        };
+
+        form.addEventListener("submit", handleSubmit);
+
+        // cleanup listener
+        return () => {
+            form.removeEventListener("submit", handleSubmit);
+        };
+        }
+    }, []);
     return(
         <>
         <section className="newsletter-section">
@@ -9,10 +42,10 @@ const NewsletterComp = () =>{
                             <h2>Dapatkan Manfaat Eksklusif</h2>
                             <p>Berlangganan ke daftar email kami dan dapatkan promosi eksklusif</p>
                         </div>
-                        <form action="https://formspree.io/f/xoqgjrok" method="POST">
+                        <form action="" method="POST" name="regist-form">
                             <div className="input_box">
-                                <input type="email" name="email" placeholder="Email"/>
-                                <button>Langganan</button>
+                                <input type="email" name="Email" placeholder="Email"/>
+                                <button type="submit">Langganan</button>
                             </div>
                         </form>
                     </div>
