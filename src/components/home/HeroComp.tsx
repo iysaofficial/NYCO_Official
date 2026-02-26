c44eee4be108ca4ac555b0d2c5ad0d415981b6af
Eki Iman <ekiimanjordiansyah212@gmail.com>
2026-02-26 11:56:41 +0700

    open regist

--- src/components/home/HeroComp.tsx
+++ src/components/home/HeroComp.tsx
@@ -7,10 +7,11 @@ const HeroComp = () =>{
             <div className="hero-container">
                 <img src="/assets/images/logo/NYCO.png" alt=""/>
                 <h1>National Youth Chemistry Olympiad</h1>
-                {/* <p>Pendaftaran National Youth Chemistry Olympiad telah dibuka! Bergabunglah <br /> dengan acara bergengsi ini dan dapatkan pengalaman yang tak terlupakan!
-                </p> */}
-                 <br />
-                <Link href="#" target="_blank" className="registration-button">Segera Hadir 2026</Link>
+                <p>Pendaftaran National Youth Chemistry Olympiad telah dibuka! Bergabunglah <br /> dengan acara bergengsi ini dan dapatkan pengalaman yang tak terlupakan!
+                </p>
+                <br />
+                <Link href="/registration" className="registration-button">Registrasi Disini!</Link>
+                <Link href="https://drive.google.com/file/d/1nBKPHGaZN7HgW-O9uxCK_Z4WjljSeePa/preview" target="_blank" className="registration-button">GuideBook</Link>
             </div>
         </section>
         </> 
